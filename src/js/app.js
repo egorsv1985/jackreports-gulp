9c4a5a7bb1f6865079448ac42017bfaf8ad194bc
egorsv1985 <egorvs2009@gmail.com>
2022-02-22 10:54:17 +0300

    fix bags in chart.js legends in two table

--- src/js/app.js
+++ src/js/app.js
@@ -5,33 +5,33 @@ const myChart = new Chart(ctx, {
         labels: ['Jun', 'Dec', 'Nov', 'Oct', 'Sep', 'Aug'],
         datasets: [
             /* Параметры первой таблицы для IOS*/
-        {
-            label: '',
-            data: [11,11,11,11,11,11],
-            backgroundColor: [
-                '#0073FF',
-            ],
-            borderColor: [
-                '#0073FF',
-            ],
-            borderWidth: 1
-        },
+            {
+                label: '',
+                data: [11, 11, 11, 11, 11, 11],
+                backgroundColor: [
+                    '#0073FF',
+                ],
+                borderColor: [
+                    '#0073FF',
+                ],
+                borderWidth: 1
+            },
             /* Параметры первой таблицы для Android*/
-        {
-            label: '',
-            data: [7,7,7,7,7,7],
-            backgroundColor: [
-                '#009E8B',
-            ],
-            borderColor: [
-                '#009E8B',
-            ],
-            borderWidth: 1,
-            
-        },
-        
-    ],
-    
+            {
+                label: '',
+                data: [7, 7, 7, 7, 7, 7],
+                backgroundColor: [
+                    '#009E8B',
+                ],
+                borderColor: [
+                    '#009E8B',
+                ],
+                borderWidth: 1,
+
+            },
+
+        ],
+
     },
 });
 
@@ -41,19 +41,18 @@ let chart = new Chart(ctx2, {
     type: 'line',
     data: {
         labels: ['1m', '2m', '4m', '6m', '8m', '10m', '12m'],
-        datasets: [
-        { // График зелёного цвета
-            label: 'График 1',
-            backgroundColor: 'transparent',
-            borderColor: 'green',
-            data: [70, 60, 60, 50, 40, 30, 20],
-        },
-        { // График синего цвета
-            label: 'График 2',
-            // backgroundColor: 'transparent',
-            borderColor: 'blue',
-            data: [90, 80, 80, 70, 60, 50, 40]
-        }
-    ],
+        datasets: [{ // График зелёного цвета
+                label: '',
+                backgroundColor: 'transparent',
+                borderColor: 'green',
+                data: [70, 60, 60, 50, 40, 30, 20],
+            },
+            { // График синего цвета
+                label: '',
+                // backgroundColor: 'transparent',
+                borderColor: 'blue',
+                data: [90, 80, 80, 70, 60, 50, 40]
+            }
+        ],
     },
 });
